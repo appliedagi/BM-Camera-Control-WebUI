f9e124fe2ac853869414aa2b33b04d00988af7f3
Dipan Nanda <themagicalmammal@MacBookAir.lan>
2025-07-25 17:24:47 +0100

    fix : should work

--- hotkey.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("keydown", function (e) {
-  if (e.ctrlKey && e.key.toLowerCase() === "r") {
-    e.preventDefault(); // prevent browser refresh
-    if (typeof cameras !== "undefined" && cameras[ci]) {
-      cameras[ci].toggleRecord();
-    }
-  }
-});
